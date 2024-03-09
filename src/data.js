a437b6c034fe38f23600dd3a20c1bda103ded3ef
hasbiriza <hasbiriza@gmail.com>
2024-03-09 15:01:37 +0700

    oke

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: 'Dimas Saputra',
+        tag: 'dimasmds',
+        imageUrl: '/images/images/dimasmds.jpeg',
+      },
+      {
+        id: 2,
+        name: 'Arif Faizin',
+        tag: 'arifaizin',
+        imageUrl: '/images/images/arifaizin.jpeg',
+      },
+      {
+        id: 3,
+        name: 'Rahmat Fajri',
+        tag: 'rfajri27',
+        imageUrl: '/images/images/rfajri27.jpeg',
+      },
+    ];
+   }
+    
+   export { getData };
